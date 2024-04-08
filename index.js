eb99e98a2c2e24d113dcc6e31c6ca8a42419f839
KinatoF <jromain040@gmail.com>
2024-04-08 17:37:09 +0200

    Merge Bdd and first API request

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import { initBDD } from './bdd.js';
+import axios from 'axios';
 
+const API_URL = 'http://ip-api.com/json/'
 const app = express();
 const port = 8080;
-app.get('/', (req, res) => {
-    res.send('Hello Word!')
+
+async function getIP(ip) {
+    return await axios.get(API_URL + ip)
+}
+
+app.get('/', async (req, res) => {
+    let ret = await getIP("147.210.204.186")
+    res.send(ret.data)
 })
 
 app.listen(port, async () => {
     console.log(`Example app listening on port ${port}`)
-    await initBDD();
+    await initBDD()
 })
